84e6e07328bbdfe1aa3b6d5e5d49f5105eeae769
Ayama11 <ayamalla882@gmail.com>
2024-06-24 11:01:03 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/test2ptc/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test2ptc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Test2Ptc());
+}
+
+class Test2Ptc extends StatelessWidget {
+  const Test2Ptc({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
